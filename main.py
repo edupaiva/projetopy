3c890e02f767a1a0ed1ee4eca5f86a7cece2989c
edupaiva <pro.eduardo@hotmail.com>
2021-09-14 22:19:28 -0300

    Refatoração para mais localidades

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
-location=open('acarau.txt','r')
-values=location.readlines()
-sum =0
-avg=0
-count=0
 
-for v in values:
-	    count+=1
-	    sum+=float(v)
 
-avg = round(sum/count,0)
+@app.get("/{local}")
+async def get_data(local):
+        location=open(local+'.txt','r')
+        values=location.readlines()
+        sum =0
+        avg=0
+        count=0
 
-sample_list = values
-converted_list = []
+        for v in values:
+	        count+=1
+	        sum+=float(v)
 
-for element in sample_list:
-    converted_list.append(element.strip())
+        avg = round(sum/count,0)
 
-@app.get("/acarau")
-async def get_data():
-    return {"valores": converted_list, "quant" : count, "media" : avg , "soma" : round(sum, 0)  }
+        sample_list = values
+        converted_list = []
+
+        for element in sample_list:
+            converted_list.append(element.strip())
+        
+        return {"valores": converted_list, "quant" : count, "media" : avg , "soma" : round(sum, 0)  }
